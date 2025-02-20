07ae2e7d4f74a26aad5841d05a1cf8ddcc4e5b7b
Bunniphat.l <shuffer2543@gmail.com>
2025-02-20 17:45:42 +0700

    try to fix LCP

--- app/layout.js
+++ app/layout.js
@@ -20,11 +20,7 @@ export default function RootLayout({ children }) {
         <link rel="preload" href="/image/Club/Augsburg_Transparent.webp" as="image"/>
         <link rel="preload" href="/image/Club/AS_Monaco_Transparent.webp" as="image"/>
         <link rel="preload" href="/image/Video/herosectionVideo.webm" as="video" />
-        <link
-            rel="preload"
-            href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100;300;400;500;700;800;900&display=swap"
-            as="style"
-        />
+        <link rel="preload" href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100;300;400;500;700;800;900&display=swap" as="style"/>
       </head>
       <body>
         <Navbar />
