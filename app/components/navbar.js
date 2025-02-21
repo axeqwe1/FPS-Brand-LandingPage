0212af8d14f43e0c7557846db70b2e53883c3ea0
Bunniphat.l <shuffer2543@gmail.com>
2025-02-21 11:24:17 +0700

    fix color dropdown menu

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -32,13 +32,13 @@ const NavBar = ({ scrollToSection }) => {
             <div className="dropdown">
               <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="black"  // กำหนดสีเส้นเป็นสีดำ
+                    >
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
                 </svg>
               </div>
               <ul
